274f77002d48b717f9cb57bb70981e8bd9a3e635
samael <samael.65535@gmail.com>
2011-12-25 21:18:21 +0800

    添加panel模块

---
+++ Panel.py
@@ -0,0 +1,18 @@
+#-*- encoding: utf-8 -*-
+
+import wx, sys, noname
+from database import *
+
+class Add_depa(noname.add_depa_panel):
+    pass
+
+class Modify_depa(noname.modify_depa_panel):
+    pass
+
+class Del_depa(noname.del_depa_panel):
+    pass
+
+
+
+
+
